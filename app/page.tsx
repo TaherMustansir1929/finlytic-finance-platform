38ca17c1a64d25e0b8c920c91a13c01a78c6f1ea
TaherMustansir1929 <eren.jeager1929101@gmail.com>
2025-07-15 20:39:24 +0500

    03.1: Implement Hero Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import HeroSection from "@/components/Hero";
+
 export default function Home() {
   return (
-    <div className="mt-40">Hello world</div>
+    <div className="mt-40">
+      <HeroSection />
+    </div>
   );
 }
