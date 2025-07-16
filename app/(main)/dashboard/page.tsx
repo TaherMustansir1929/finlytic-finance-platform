761c24125d6f012c2422330c1caf4cba51bc9911
TaherMustansir1929 <eren.jeager1929101@gmail.com>
2025-07-16 16:05:26 +0500

    06.2: Implement Form UI

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -1,3 +1,7 @@
+import CreateAccountDrawer from "@/components/CreateAccountDrawer";
+import { Card, CardContent } from "@/components/ui/card";
+import { Plus } from "lucide-react";
+
 function DashboardPage() {
   return (
     <div className="px-5">
@@ -6,8 +10,17 @@ function DashboardPage() {
       {/* Overview */}
 
       {/* Accounts Grid */}
-      
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <CreateAccountDrawer>
+          <Card className="hover:shadow-md transition-shadow duration-200 cursor-pointer border-dashed">
+            <CardContent className="flex flex-col items-center justify-center h-full text-muted-foreground pt-5">
+              <Plus className="w-10 h-10 mb-2" />
+              <p className="text-sm font-medium">Add New Account</p>
+            </CardContent>
+          </Card>
+        </CreateAccountDrawer>
+      </div>
     </div>
   );
-};
+}
 export default DashboardPage;
